c52a87b6bf23cd85d6448d10ddb1c8a18937b645
7sendoh <ringo@Pings-Mac-mini.local>
2016-12-30 13:21:59 -0500

    Initial Commit

---
+++ rainingshinnycloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainingshinnycloudy
+//
+//  Created by Ping Li on 2016-12-30.
+//  Copyright © 2016 Ping Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
